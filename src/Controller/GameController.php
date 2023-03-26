63081a0fc1440681a30d1aa56b8f017a28c3bcab
Nike555 <zubcu_nike@mail.ru>
2023-03-26 03:58:30 +0300

    Add on page possibility to play game if current user already not played it, and hide button for play if user was already played this game.

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,17 +2,67 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Entity\UserGamePrize;
+use App\Form\UserPlayGameFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class GameController extends AbstractController
 {
-    #[Route('/game', name: 'game')]
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    )
+    {}
+
+    #[Route('/game', name: 'game', methods: ['GET'])]
     public function index(): Response
     {
+        $userCanPlay = $this->userCanPlay();
+        $userCurrentGamePrize = $this->entityManager->getRepository(UserGamePrize::class)->getUserGamePrize($this->getUser());
+        $userGamePrize = new UserGamePrize();
+        $form = $this->createForm(UserPlayGameFormType::class, $userGamePrize);
+
         return $this->render('game/index.html.twig', [
-            'controller_name' => 'GameController',
+            'playGameForm' => $form->createView(),
+            'user_can_play' => $userCanPlay,
+            'user_current_game_prize' => $userCurrentGamePrize,
         ]);
+    }
+
+    #[Route('/game', name: 'game_get_reward', methods: ['POST'])]
+    public function getReward(
+        Request $request,
+        EventDispatcherInterface $eventDispatcher
+    ): Response
+    {
+        $userGamePrize = new UserGamePrize();
+        $form = $this->createForm(UserPlayGameFormType::class, $userGamePrize);
+        $form->handleRequest($request);
+
+        if ($this->userCanPlay() && $form->isSubmitted() && $form->isValid()) {
+            $userGamePrizeData = $form->getData();
+            dump($userGamePrizeData);
+
+            //$eventDispatcher->dispatch(new CommentCreatedEvent($userGamePrize));
+
+            //return $this->redirectToRoute('blog_post', ['slug' => $post->getSlug()]);
+        }
+        return $this->redirectToRoute('game');
+    }
+
+    /**
+     * Check if current user can play game
+     * @return bool
+     */
+    private function userCanPlay(): bool
+    {
+        return $this->entityManager->getRepository(UserGamePrize::class)->checkUserCanPlay($this->getUser());
     }
 }
--- src/Repository/UserGamePrizeRepository.php
+++ src/Repository/UserGamePrizeRepository.php
@@ -2,9 +2,15 @@
 
 namespace App\Repository;
 
+use App\Entity\Game;
+use App\Entity\Partner;
+use App\Entity\Prize;
+use App\Entity\User;
 use App\Entity\UserGamePrize;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @extends ServiceEntityRepository<UserGamePrize>
@@ -16,9 +22,12 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class UserGamePrizeRepository extends ServiceEntityRepository
 {
+    private $currentDay;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, UserGamePrize::class);
+        $this->currentDay = (new \DateTime())->format('Y-m-d');
     }
 
     public function save(UserGamePrize $entity, bool $flush = false): void
@@ -39,30 +48,46 @@ class UserGamePrizeRepository extends ServiceEntityRepository
         }
     }
 
+    public function checkUserCanPlay(UserInterface $user): bool
+    {
+        $userCanPlay = $this->createQueryBuilder('ugp')
+            ->select('COUNT(ugp.id)')
+            ->leftJoin(User::class, 'u', Join::WITH, 'u.id = ugp.user')
+            ->leftJoin(Game::class, 'g', Join::WITH, 'g.id = ugp.game')
+            ->where(':current_day BETWEEN g.start_date AND g.end_date')
+            ->andWhere('ugp.user = :user_id')
+            ->setParameter('user_id', $user->getId())
+            ->setParameter('current_day', $this->currentDay)
+            ->getQuery()
+            ->getSingleScalarResult();
+        return !$userCanPlay;
+    }
 
+    public function getUserGamePrize(UserInterface $user): ?UserGamePrize
+    {
+        $userLanguageId = $user->getLanguage()->getId();
 
-//    /**
-//     * @return UserGamePrize[] Returns an array of UserGamePrize objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+        $gameInfo = $this->createQueryBuilder('ugp')
+            ->select('ugp, pt')
+            ->leftJoin(User::class, 'u', Join::WITH, 'u.id = ugp.user')
+            ->leftJoin(Game::class, 'g', Join::WITH, 'g.id = ugp.game')
+            ->leftJoin(Prize::class, 'pr', Join::WITH, 'pr.id = ugp.prize')
+            ->leftJoin(Partner::class, 'pt', Join::WITH, 'pt.code = pr.partner_code')
+            ->where(':current_day BETWEEN g.start_date AND g.end_date')
+            ->andWhere('ugp.user = :user_id')
+            ->andWhere('pt.language = :language_id')
+            ->setParameter('user_id', $user->getId())
+            ->setParameter('current_day', $this->currentDay)
+            ->setParameter('language_id', $userLanguageId)
+            ->getQuery()
+            ->execute();
 
-//    public function findOneBySomeField($value): ?UserGamePrize
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        if ($gameInfo !== null) {
+            if (!empty($gameInfo[1])) {
+                $gameInfo[0]->partner = $gameInfo[1];
+            }
+            return $gameInfo[0];
+        }
+        return null;
+    }
 }
