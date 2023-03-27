d0307b58ed1083fcd6f831c34a735915c369629d
Nike555 <zubcu_nike@mail.ru>
2023-03-27 23:30:09 +0300

    Optimize GameService

--- src/Service/GameService.php
+++ src/Service/GameService.php
@@ -3,18 +3,23 @@
 namespace App\Service;
 
 use App\Entity\Game;
+use App\Repository\GameRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class GameService
 {
+    private GameRepository $gameRepository;
+
     public function __construct(
         private EntityManagerInterface $entityManager,
     )
-    {}
+    {
+        $this->gameRepository = $this->entityManager->getRepository(Game::class);
+    }
 
     public function getAllGames(): array
     {
-        $games = $this->entityManager->getRepository(Game::class)->getAllGames();
+        $games = $this->gameRepository->getAllGames();
         $data = [];
 
         if ($games) {
